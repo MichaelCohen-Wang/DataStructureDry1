a4a2d2551697a24bddc8d1dad5e1a452c1b2822c
Michael Cohen-Wang <142043002+MichaelCohen-Wang@users.noreply.github.com>
2024-07-07 15:51:44 +0000

    fixed errors

--- DSwet1S24/code/AVLNode.h
+++ DSwet1S24/code/AVLNode.h
@@ -12,21 +12,98 @@ private:
     T val; //node will hold pirate or ship
     int key; 
 
-    AVLNode(int valueId);
+    AVLNode(int valueId, T valueId);
 
     ~AVLNode(); 
 
     void setLeftNode(AVLNode* left); 
     void setRightNode(AVLNode* right);
 
-    int findBalanceFactor();
+    int findBalanceFactor() const;
     void fixValues(); //used to update values after rotation
 
 
-    AVLNode* rotateLeft(); //LL rotation
-    AVLNode* rotateRight(); //RR rotation
+    //AVLNode* rotateLeft(); //LL rotation
+    //AVLNode* rotateRight(); //RR rotation
+    void rotateLeft();
+    void rotateRight(); 
 
 
 
 
 };
+
+template<class T>
+AVLNode<T>::AVLNode(int valueId, T value): value(value){
+leftNode = nullptr ;
+rightNode = nullptr; 
+count = 1;
+height = 1;
+key = valueId; 
+}
+
+template<class T>
+AVLNode<T>::~AVLNode() = default; 
+
+template<class T>
+void AVLNode<T>::setLeftNode(AVLNode* left){
+    leftNode = left; 
+}
+
+template<class T>
+void AVLNode<T>::setRightNode(AVLNode* right){
+    rightNode = right; 
+}
+
+template<class T>
+int AVLNode<T>::findBalanceFactor() const{
+    int left; 
+    int right; 
+    if(leftNode == nullptr){
+        left = 0; 
+    }
+    else{
+        left = leftNode.height;
+    }
+
+    if(rightNode == nullptr){
+        right = 0; 
+    }
+    else{
+        right = rightNode.height;
+    }
+
+    return left- right; 
+}
+
+template<class T>
+void AVLNode<T>::fixValues(){ 
+    {
+    count = (leftNode != nullptr ? leftNode->count : 0) + (rightNode != nullptr ? rightNode->count : 0) + 1;
+    
+    height = std::max(leftNode != nullptr ? leftNode->height : 0, rightNode != nullptr ? rightNode->height : 0) + 1;
+}
+}
+
+//for this function to work, rightNode needs a leftNode
+//check notebook diagram
+template<class T>
+void AVLNode<T>::rotateLeft(){ //BF(v) = 2, BF(v_L) = -1
+    AVLNode* right = rightNode; //holds onto rightNode even after reassignment
+    this->rightNode = rightNode-> leftNode; //rightNode becomes rightNode's (bigger than this) leftNode (bigger than this but smaller than rightNode) 
+    right -> leftNode  = this; 
+    this.fixValues();
+    right.fixValues()
+}
+
+//for this function to work, leftNode needs a rightNode
+//same as rotateLeft but switches right and left
+template<class T>
+void AVLNode<T>::rotateRight(){
+    AVLNode* left = leftNode; //holds onto rightNode even after reassignment
+    this->leftNode = leftNode-> rightNode; //rightNode becomes rightNode's (bigger than this) leftNode (bigger than this but smaller than rightNode) 
+    left -> rightNode  = this; 
+    this.fixValues();
+    left.fixValues()
+
+}
